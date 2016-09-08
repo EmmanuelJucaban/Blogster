fa367e599abf22e5f3cad002c4f052e10650a26d
emmanueljucaban <emmanueljucaban@.com>
2016-09-08 10:42:47 +0000

    Added article edit and show page

--- app/models/articles.rb
+++
@@ -1,5 +0,0 @@
-class Articles < ActiveRecord::Base
-  validates :title, presense: true, length: {minimum: 5, maximum: 50}
-  validates :description, presense: true, length: {minimum: 10, maximum: 300}
-  
-end
