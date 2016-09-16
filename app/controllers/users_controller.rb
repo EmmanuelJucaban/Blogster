501dc85d44635a4ab976c17e91740290ec3b5953
emmanueljucaban <emmanueljucaban@.com>
2016-09-16 10:30:04 +0000

    Add signin upon sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,9 @@ class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to Blogster #{@user.username}!"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
