62208726411ea48c40adcd227a60d94bc058ebc5
emmanueljucaban <emmanueljucaban@gmail.com>
2016-09-17 18:20:25 +0000

    Add edit functionality to category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -31,7 +31,7 @@ class CategoriesController < ApplicationController
     @category = Category.find(params[:id])
     if @category.update(category_params)
       flash[:success] = "Category name was successfully updated"
-      redirect_to categories_path
+      redirect_to category_path(@category)
     else
       render 'edit'
     end
